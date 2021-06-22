3e4201f469ea2da978212057208c639f03b471ba
pareshsahoo902 <pareshsahoo902@gmail.com>
2021-06-22 22:14:55 +0530

    DemoPush Commmmmmmit

--- app/src/main/java/com/release/androidbatch2/Login/DemoPush.java
+++ app/src/main/java/com/release/androidbatch2/Login/DemoPush.java
@@ -3,4 +3,17 @@ package com.release.androidbatch2.Login;
 public class DemoPush {
 
 //He;;wewfwwwwwwwwwwwwwwwwwwwwwwwwwww
+    String y;
+
+    public DemoPush(String y) {
+        this.y = y;
+    }
+
+    public String getY() {
+        return y;
+    }
+
+    public void setY(String y) {
+        this.y = y;
+    }
 }
